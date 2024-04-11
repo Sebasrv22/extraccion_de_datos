5dc2b48f35a28bc7a955835861cb965f94302ea9
Sebasrv22 <a01656563@tec.mx>
2024-04-10 19:54:49 -0600

    filtro7

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -37,8 +37,18 @@ filtro4.to_csv("Practica_facturacion4.csv")
 filtro5 = df.iloc[:, [0, 6, 7, 9]]
 print(filtro5)
 filtro5.to_csv("Practica_facturacion5.csv")
-               
-#filtro6 = df.iloc[7001:7003, [0, 1, 2]]
-#print(filtro6)
-#filtro6.to_csv("Practica_facturacion6.csv)
-               
+
+#filtro 6
+filtro6 = df.iloc[7001:7003, [0, 1, 2]]
+print(filtro6)
+filtro6.to_csv("Practica_facturacion6.csv")
+
+#filtro7
+#df1 = pd.read_excel('datos_facturacion.xlsx', index_col=3)
+#filtro7.to_csv("Practica_facturacion7.csv")
+#print(df1.head())
+
+
+
+
+
