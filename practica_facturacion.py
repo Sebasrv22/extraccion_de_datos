e608ba313f728778d0a3bae395b933da905e388d
Sebasrv22 <a01656563@tec.mx>
2024-04-10 19:20:18 -0600

    filtro 3

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -14,12 +14,20 @@ filtro2 = df[ ~(df["CVE_VEND"] == 5.0) & ~(df["CVE_VEND"] == 4.0) ]
 filtro2.to_csv("Practica_facturacion2.csv")
 
 # filtro 3
-#filtro3=df[df["FECHA_ENT"] == '2022-28-2']
-#print(filtro3)
+# year
+#filtro3=df[ df["FECHA_ENT"].dt.strftime('%Y') == '2019']
+# year-month
+#filtro3=df[ df["FECHA_ENT"].dt.strftime('%Y-%m') == '2019-10']
+# year-month-day
+filtro3=df[ df["FECHA_ENT"].dt.strftime('%Y-%m-%d') == '2019-10-02']
+
+print(filtro3)
 
 # Filtro 4: CAN_TOT debe ser menor que 5951.7 o STATUS debe ser 'E'
 filtro4 = (df['CAN_TOT'] < 5951.7) | (df['STATUS'] == 'E')
 print(filtro4)
 filtro4.to_csv("Practica_facturacion4.csv")
 
-
+# Filtro 5: Seleccionamos solo las columnas especificadas
+filtro5 = ['CVE_DOC', 'FECHA_ENT', 'FECHA_VEN', 'CAN_TOT']
+print(filtro5)
