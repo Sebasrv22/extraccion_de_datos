aee07af725b6669db2bbcc8e3f34ff1953042c64
Sebasrv22 <a01656563@tec.mx>
2024-04-10 18:54:15 -0600

    filtro4

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -10,5 +10,16 @@ filtro1.to_csv("Practica_facturacion1.csv")
 
 # Filtro 2: CVE_VEND no debe ser 5 ni 4
 filtro2 = df[ ~(df["CVE_VEND"] == 5.0) & ~(df["CVE_VEND"] == 4.0) ]
-print(filtro2)
+#print(filtro2)
 filtro2.to_csv("Practica_facturacion2.csv")
+
+# filtro 3
+#filtro3=df[df["FECHA_ENT"] == '2022-28-2']
+#print(filtro3)
+
+# Filtro 4: CAN_TOT debe ser menor que 5951.7 o STATUS debe ser 'E'
+filtro4 = (df['CAN_TOT'] < 5951.7) | (df['STATUS'] == 'E')
+print(filtro4)
+filtro4.to_csv("Practica_facturacion4.csv")
+
+
